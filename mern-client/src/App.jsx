b8a04096de00ece03e0b1de95c990b7cb39132d8
imancrrzii <imancarrazi777@gmail.com>
2024-06-19 19:44:44 +0700

    feat: initial project, tailwind, daisy ui, react, for client

---
+++ mern-client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='min-h-screen w-full flex justify-center text-red-400 font-semibold font-primary '>
+        apa benar
+      </div>
+    </>
+  )
+}
+
+export default App
