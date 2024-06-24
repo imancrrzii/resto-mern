6f25285b4757220bf2f88a8e70eccb7a221e845a
imancrrzii <imancarrazi777@gmail.com>
2024-06-24 09:34:53 +0700

    feat: add login and register page using reach-hook-form

--- mern-client/src/components/Modal.jsx
+++ mern-client/src/components/Modal.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { FaFacebook, FaGithub, FaGoogle } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const Modal = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
   return (
     <dialog id="my_modal_5" className="modal modal-middle sm:modal-middle">
       <div className="modal-box">
         <div className="modal-action flex flex-col justify-center mt-0">
-          <form className="card-body" method="dialog">
+          <form
+            className="card-body"
+            method="dialog"
+            onSubmit={handleSubmit(onSubmit)}
+          >
             <h3 className="font-bold text-lg">Login now</h3>
             <div className="form-control">
               <label className="label">
@@ -18,6 +29,7 @@ const Modal = () => {
                 placeholder="email"
                 className="input input-bordered"
                 required
+                {...register("email")}
               />
             </div>
             <div className="form-control">
@@ -29,6 +41,7 @@ const Modal = () => {
                 placeholder="password"
                 className="input input-bordered"
                 required
+                {...register("password")}
               />
               <label className="label mt-1">
                 <a href="#" className="label-text-alt link link-hover">
@@ -51,6 +64,13 @@ const Modal = () => {
                 Register now
               </Link>
             </p>
+            <button
+              html_for="my_modal_5"
+              onClick={() => document.getElementById("my_modal_5").close()}
+              className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+            >
+              ✕
+            </button>
           </form>
           {/* social login */}
           <div className="text-center space-x-3 mb-5 ">
--- mern-client/src/router/Router.jsx
+++ mern-client/src/router/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../pages/home/Home";
 import Menu from "../pages/shop/Menu";
+import Register from "../components/Register";
 
 const router = createBrowserRouter([
   {
@@ -18,6 +19,10 @@ const router = createBrowserRouter([
       },
     ],
   },
+  {
+    path: "/register",
+    element: <Register/>,
+  }
 ]);
 
 export default router;
