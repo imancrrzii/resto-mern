4cef34c2bfeb4ed08db55814a913392b7ce4bea2
imancrrzii <imancarrazi777@gmail.com>
2024-06-28 16:57:34 +0700

    feat: add create user controller

--- mern-server/api/controllers/userControllers.js
+++ mern-server/api/controllers/userControllers.js
@@ -1,12 +1,28 @@
 const User = require("../models/User");
 
 const getUsers = async (req, res) => {
-    try {
-        const users = await User.find({});
-        res.status(200).json(users);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-}
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-module.exports = { getUsers }
+// create user
+const createUser = async (req, res) => {
+  const user = req.body;
+  const query = { email: user.email };
+  try {
+    const existingUser = await User.findOne(query);
+    if (existingUser) {
+      return res.status(302).json({ message: "User already exists" });
+    }
+    const result = await User.create(user);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { getUsers, createUser };
--- mern-server/api/routes/userRoutes.js
+++ mern-server/api/routes/userRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const userController = require('../controllers/userControllers')
 
 router.get('/', userController.getUsers);
+router.post('/', userController.createUser);
 
 module.exports = router
