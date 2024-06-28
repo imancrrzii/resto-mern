9ba07d8fc513c85d84f718fd0a4d66fe864e365f
imancrrzii <imancarrazi777@gmail.com>
2024-06-28 20:34:12 +0700

    feat: add get admin function controller

--- mern-server/api/controllers/userControllers.js
+++ mern-server/api/controllers/userControllers.js
@@ -38,4 +38,42 @@ const deleteUser = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
-module.exports = { getUsers, createUser, deleteUser };
+
+// get admin
+const getAdmin = async (req, res) => {
+  const email = req.query.email;
+  const query = { email: email };
+  try {
+    const user = await User.findOne(query);
+    if (email !== req.decoded.email) {
+      return res.status(403).send({ message: "Forbidden Access" });
+    }
+    let admin = false;
+    if (user) {
+      admin = user?.role === "admin";
+    }
+    res.status(200).send({ admin });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// make admin of user
+const makeAdmin = async (req, res) => {
+  const userId = req.params.id;
+  const { name, email, role, photoURL } = req.body;
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { role: "admin" },
+      { new: true, runValidators: true }
+    );
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+module.exports = { getUsers, createUser, deleteUser, getAdmin, makeAdmin };
