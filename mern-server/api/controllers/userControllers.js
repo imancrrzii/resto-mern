a4f347ef495ea6b92577d527527d2e7782552e45
imancrrzii <imancarrazi777@gmail.com>
2024-06-28 17:00:16 +0700

    feat: add delete user function controller

--- mern-server/api/controllers/userControllers.js
+++ mern-server/api/controllers/userControllers.js
@@ -25,4 +25,17 @@ const createUser = async (req, res) => {
   }
 };
 
-module.exports = { getUsers, createUser };
+// delete user
+const deleteUser = async (req, res) => {
+  const userId = req.params.id;
+  try {
+    const deletedUser = await User.findByIdAndDelete(userId);
+    if (!deletedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({ message: "User deleted successfully!" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+module.exports = { getUsers, createUser, deleteUser };
